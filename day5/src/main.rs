042692d5e8116f492c94c1ede5350d3b0c5c7fc1
Thomas Beyer <thomas.beyer@capellaspace.com>
2024-12-16 21:06:53 -0800

    day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs;
 
 const DAY: &str = "day5";
+const MISSING_TRUE: bool = true;
 
 #[derive(Debug, Clone)]
 struct Node {
@@ -10,38 +11,73 @@ struct Node {
     pages_after: Vec<i32>,
 }
 
+#[derive(Debug, Clone)]
+struct SleighLaunchSafetyManual {
+    page_order_rules: HashMap<i32, Node>,
+    page_number_updates: Vec<Vec<i32>>,
+}
+
+impl SleighLaunchSafetyManual {
+    fn page_updates_right_order(&self, page_updates: &Vec<i32>) -> bool {
+        for pos in 0..page_updates.len() {
+            let result: (bool, i32) = self._check_single(page_updates, pos);
+            if !result.0 {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn _check_single(&self, page_updates: &Vec<i32>, pos: usize) -> (bool, i32) {
+        let cur: i32 = page_updates[pos];
+
+        if !self.page_order_rules.contains_key(&cur) {
+            return (MISSING_TRUE, -1);
+        }
+
+        let pre = &page_updates[..pos];
+        let post = &page_updates[pos + 1..];
+        let node: &Node = self.page_order_rules.get(&cur).unwrap();
+
+        for cur_pre in pre.iter() {
+            if node.pages_after.contains(&cur_pre) {
+                return (false, *cur_pre);
+            }
+        }
+        for cur_post in post.iter() {
+            if !node.pages_after.contains(&cur_post) {
+                return (false, *cur_post);
+            }
+        }
+        return (true, 1);
+    }
+
+    fn page_updates_problem(&self, page_updates: &Vec<i32>) {
+        for pos in 0..page_updates.len() {
+            let result: (bool, i32) = self._check_single(page_updates, pos);
+            if !result.0 {
+                println!("{} is problem of {:?}", result.1, page_updates);
+            }
+        }
+    }
+}
+
 fn main() {
     println!("TEST");
     let test_file_path = format!("/{}/src/{}_test.txt", DAY, DAY);
 
-    let mut print_rules: HashMap<i32, Node> = HashMap::new();
-
     let instrs_str = &read_lines(&test_file_path);
-    for line in instrs_str.lines() {
-        let instr: Vec<_> = line.split('|').map(|i| i.parse::<i32>().unwrap()).collect();
-        let mut node: Node = Node {
-            page: instr[0],
-            pages_after: vec![instr[1]],
-        };
-        if !print_rules.contains_key(&node.page) {
-            print_rules.insert(node.page, node);
-        } else {
-            let mut existing = &print_rules[&node.page];
-            node.pages_after.extend(existing.pages_after.clone());
-            print_rules.insert(node.page, node);
-        }
-    }
-    println!("{:?}", print_rules);
+    let manual = parse(&instrs_str);
 
-    // let word_search = WordSearch::new(&read_lines(&test_file_path), word.clone());
-    // part1(&word_search);
-    // part2(&word_search);
+    part1(&manual);
+    part2(&manual);
 
-    // println!("FOR REAL");
-    // let file_path = format!("/{}/src/{}.txt", DAY, DAY);
-    // let word_search = WordSearch::new(&read_lines(&file_path), word.clone());
-    // part1(&word_search);
-    // part2(&word_search);
+    println!("FOR REAL");
+    let file_path = format!("/{}/src/{}.txt", DAY, DAY);
+    let instrs_str = &read_lines(&file_path);
+    let manual = parse(&instrs_str);
+    part1(&manual);
+    part2(&manual);
 }
 
 fn read_lines(filename: &str) -> String {
@@ -51,206 +87,64 @@ fn read_lines(filename: &str) -> String {
     return contents;
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Coord {
-    y: i32, // row
-    x: i32, // col
+fn parse(instrs_str: &String) -> SleighLaunchSafetyManual {
+    let empty: Vec<_> = instrs_str
+        .lines()
+        .enumerate()
+        .filter(|(i, l)| l.is_empty())
+        .collect();
+
+    let split_at: usize = empty[0].0;
+    let lines: Vec<_> = instrs_str.lines().collect();
+
+    let mut page_order_rules: HashMap<i32, Node> = HashMap::new();
+    for line in &lines[..split_at] {
+        let instr: Vec<_> = line.split('|').map(|i| i.parse::<i32>().unwrap()).collect();
+        let mut node: Node = Node {
+            page: instr[0],
+            pages_after: vec![instr[1]],
+        };
+        if !page_order_rules.contains_key(&node.page) {
+            page_order_rules.insert(node.page, node);
+        } else {
+            let mut existing = &page_order_rules[&node.page];
+            node.pages_after.extend(existing.pages_after.clone());
+            page_order_rules.insert(node.page, node);
+        }
+    }
+
+    let mut page_number_updates: Vec<Vec<i32>> = Vec::new();
+    for line in &lines[split_at + 1..] {
+        let page: Vec<_> = line.split(',').map(|i| i.parse::<i32>().unwrap()).collect();
+        page_number_updates.push(page);
+    }
+    return SleighLaunchSafetyManual {
+        page_order_rules: page_order_rules,
+        page_number_updates: page_number_updates,
+    };
 }
 
-fn get_offsets() -> Vec<Coord> {
-    return vec![
-        Coord { y: -1, x: 0 },
-        Coord { y: -1, x: 1 },
-        Coord { y: 0, x: 1 },
-        Coord { y: 1, x: 1 },
-        Coord { y: 1, x: 0 },
-        Coord { y: 1, x: -1 },
-        Coord { y: 0, x: -1 },
-        Coord { y: -1, x: -1 },
-    ];
+fn part1(manual: &SleighLaunchSafetyManual) {
+    let mut middle_page_acc: i32 = 0;
+    for page_updates in manual.page_number_updates.iter() {
+        if manual.page_updates_right_order(&page_updates) {
+            // get middl&e
+            middle_page_acc += page_updates[page_updates.len() / 2 as usize];
+        }
+    }
+    println!("part1: {:?}", middle_page_acc);
 }
 
-fn get_patch_offsets() -> Vec<Coord> {
-    return vec![
-        Coord { y: -1, x: -1 },
-        Coord { y: -1, x: 0 },
-        Coord { y: -1, x: 1 },
-        Coord { y: 0, x: -1 },
-        Coord { y: 0, x: 0 },
-        Coord { y: 0, x: 1 },
-        Coord { y: 1, x: -1 },
-        Coord { y: 1, x: 0 },
-        Coord { y: 1, x: 1 },
-    ];
-}
-
-#[derive(Debug, Clone, Copy)]
-struct WordSearchChar {
-    coord: Coord,
-    letter: char,
-}
-
-#[derive(Debug)]
-struct WordSearch {
-    _data: Vec<Vec<WordSearchChar>>,
-    word: Vec<char>,
-}
-
-impl WordSearch {
-    fn new(word_search_str: &str, word: Vec<char>) -> Self {
-        let mut _data: Vec<Vec<WordSearchChar>> = Vec::new();
-
-        for (i, line) in word_search_str.lines().enumerate() {
-            let mut cur_line: Vec<WordSearchChar> = Vec::new();
-            for (j, cur) in line.chars().enumerate() {
-                cur_line.push(WordSearchChar {
-                    coord: Coord {
-                        y: i as i32,
-                        x: j as i32,
-                    },
-                    letter: cur,
-                });
-            }
-            _data.push(cur_line);
-        }
-        return Self { _data, word };
-    }
-
-    fn word_size(&self) -> usize {
-        return self.word.len();
-    }
-
-    fn word_str(&self) -> String {
-        return self.word.iter().collect();
-    }
-
-    fn rows(&self) -> i32 {
-        return self._data.len() as i32;
-    }
-
-    fn cols(&self) -> i32 {
-        return self._data[0].len() as i32;
-    }
-
-    fn in_bounds(&self, coord: Coord) -> bool {
-        return coord.y >= 0 && coord.y < self.rows() && coord.x >= 0 && coord.x < self.cols();
-    }
-
-    fn at(&self, coord: Coord) -> WordSearchChar {
-        return self._data[coord.y as usize][coord.x as usize];
-    }
-
-    fn find_all(&self) -> u32 {
-        let mut total: u32 = 0;
-        for y in 0..self.rows() {
-            for x in 0..self.cols() {
-                total += self.get_match_count_at(Coord {
-                    y: y as i32,
-                    x: x as i32,
-                });
-            }
-        }
-        return total;
-    }
-
-    fn get_match_count_at(&self, coord: Coord) -> u32 {
-        let start = self.at(coord);
-        let slices = self.get_neighbor_slices(start);
-
-        let mut matches: u32 = 0;
-
-        for slice in slices {
-            if slice == self.word_str() {
-                matches += 1;
-            }
-        }
-
-        return matches;
-    }
-
-    fn get_neighbor_slices(&self, start: WordSearchChar) -> Vec<String> {
-        // println!("getting slices of {:?}", start);
-        let mut full_slices: Vec<String> = Vec::new();
-
-        for off in get_offsets() {
-            let mut ptr: WordSearchChar = start.clone();
-            let mut cur_slice: Vec<char> = vec![ptr.letter];
-
-            for _ in 0..self.word_size() - 1 {
-                let next_coord = Coord {
-                    y: ptr.coord.y + off.y,
-                    x: ptr.coord.x + off.x,
-                };
-                if !self.in_bounds(next_coord) {
-                    break;
-                }
-                let next = self.at(next_coord);
-                cur_slice.push(next.letter);
-                ptr = next;
-            }
-            if cur_slice.len() == self.word_size() {
-                let s: String = cur_slice.iter().collect();
-                full_slices.push(s);
-            }
-        }
-        return full_slices;
-    }
-
-    fn get_patch(&self, start: WordSearchChar) -> Vec<char> {
-        let mut patch: Vec<char> = Vec::new();
-        for off in get_patch_offsets() {
-            let next_coord = Coord {
-                y: start.coord.y + off.y,
-                x: start.coord.x + off.x,
-            };
-            if !self.in_bounds(next_coord) {
-                break;
-            }
-            let next = self.at(next_coord);
-            patch.push(next.letter);
-        }
-
-        if patch.len() != 9 {
-            return vec![];
-        }
-        return patch;
-    }
-}
-
-fn part1(word_search: &WordSearch) {
-    let match_cnt: u32 = word_search.find_all();
-
-    println!("part1: {:?}", match_cnt);
-}
-
-fn part2(word_search: &WordSearch) {
-    let mut a_coords: Vec<WordSearchChar> = Vec::new();
-
-    for row in word_search._data.clone() {
-        for it in row {
-            if it.letter == 'A' {
-                a_coords.push(it);
-            }
+fn part2(manual: &SleighLaunchSafetyManual) {
+    let mut invalid_ordered: Vec<&Vec<i32>> = Vec::new();
+    for page_updates in manual.page_number_updates.iter() {
+        if !manual.page_updates_right_order(&page_updates) {
+            invalid_ordered.push(page_updates);
         }
     }
 
-    let mut cnt: u32 = 0;
-    for a in a_coords {
-        let cur_patch = word_search.get_patch(a);
-        if cur_patch.is_empty() {
-            continue;
-        }
-
-        let mut mmss: Vec<char> = vec![cur_patch[0], cur_patch[2], cur_patch[6], cur_patch[8]];
-        mmss.sort();
-        if mmss == vec!['M', 'M', 'S', 'S'] {
-            // eliminate diagonal
-            if cur_patch[0] != cur_patch[8] {
-                println!("{:?} patch for {:?}", mmss, a);
-                cnt += 1;
-            }
-        }
+    for invalid in invalid_ordered {
+        manual.page_updates_problem(&invalid);
     }
-
-    println!("part2: {:?}", cnt);
+    println!("part1: {:?}", 0);
 }
